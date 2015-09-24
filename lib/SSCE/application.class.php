2eb55e3e0ec9cece21fb7e9a510446b1b47d6637
Vadim Suleimanov <s1imus05@gmail.com>
2015-09-24 17:02:26 +0300

    Working on _boot function

--- lib/SSCE/application.class.php
+++ lib/SSCE/application.class.php
@@ -3,6 +3,7 @@ class SSCE_Application {
     
     private $_oConfig;
     private $_sConfigFile;
+    private $_oDb;
  
     public function __construct($sConfigFile){
         $this->_sConfigFile         = $sConfigFile;
@@ -21,11 +22,28 @@ class SSCE_Application {
             }
         }
         
+        if (in_array(strtolower( ini_get('magic_quotes_gpc')), array('1', 'on'))) {
+            $_POST      = array_map('stripslashes', $_POST );
+            $_GET       = array_map('stripslashes', $_GET );
+            $_COOKIE    = array_map('stripslashes', $_COOKIE );
+        }
+        
         $this->_oConfig = new SSCE_Config($this->_sConfigFile);
         setlocale(LC_ALL , $this->getConfig()->project->locale);
+        
+        require_once $this->getConfig()->db->lib_path.'/Generic.php';
+        require_once $this->getConfig()->db->lib_path.'/Mysql.php';
+        $this->_oDb = DbSimple_Generic::connect("mysql://".$this->getConfig()->db->user.":".$this->getConfig()->db->password."@".$this->getConfig()->db->host."/".$this->getConfig()->db->database);
+        $this->getDb()->setIdentPrefix($this->getConfig()->db->table_prefix);
+        $this->getDb()->query("SET NAMES utf8;");
+        
     }
     
     public function getConfig() {
         return $this->_oConfig->getData();
+    }
+    
+    public function getDb() {
+        return $this->_oDb;
     }
 }
