d8aa1ad4dcb296f5515b0013ff450e9f62a10608
Vadim Suleimanov <me@vsul.ru>
2016-02-04 12:22:02 +0300

    Home Page fix

--- templates/default/home.php
+++ templates/default/home.php
@@ -16,32 +16,29 @@
         <div class="b-post__data">
             <ul class="nav nav-pills tabs_js" data-target="tabs_home">
                 <li role="presentation" class="active"><a href="#likes"><i class="fa fa-heart"></i>&nbsp; Мои лайки</a></li>
-                <li role="presentation"><a href="#comments"><i class="fa fa-cog"></i>&nbsp; Мои коментарии</a></li>
+                <li role="presentation"><a href="#comments"><i class="fa fa-comment"></i>&nbsp; Мои коментарии</a></li>
                 <li role="presentation"><a href="#settings"><i class="fa fa-cog"></i>&nbsp; Настройки</a></li>
             </ul>
             <p>&nbsp;</p>
             <div id="tabs_home">
                 <div id="likes">
                     <? if ($aPostList) :?>
-                        <div class="row">
                         <? foreach ($aPostList as $aPost) :?>
-                            <div class="col-sm-6 b-likepost">
-                                <div class="media">
-                                    <div class="media-left">
-                                        <a href="/post/<?=$aPost['id']?>">
-                                            <img class="media-object b-postthumb__small" src="<?=$aPost['thumb']?>" alt="<?=$aPost['title']?>">
-                                        </a>
-                                    </div>
-                                    <div class="media-body">
-                                        <div class="clearfix">
-                                            <h4 class="media-heading b-likelist__title pull-left"><a href="/post/<?=$aPost['id']?>"><?=$aPost['title']?></a></h4>
-                                        </div>
-                                        <p><span class="text-muted"><?=date2ru($aPost['like_date'], true)?></span> <a data-id="<?=$aPost['id']?>" class="btn btn-default btn-xs b-likedel"><i class="fa fa-times b-like" title="Убрать из списка"></i> убрать лайк</a></p>
-                                    </div>
+                            <div class="media b-comment">
+                                <div class="media-left">
+                                    <a href="/post/<?=$aPost['id']?>">
+                                        <img class="media-object b-postthumb__small" src="<?=$aPost['thumb']?>" alt="<?=$aPost['title']?>">
+                                    </a>
+                                </div>
+                                <div class="media-body">
+                                    <h4 class="media-heading">
+                                        <a href="/post/<?=$aPost['id']?>"><?=$aPost['title']?></a>,
+                                        <span class="text-muted"><?=date2ru($aPost['like_date'], true)?></span>
+                                    </h4>
+                                    <p><a data-id="<?=$aPost['id']?>" class="btn btn-default btn-sm b-likedel"><i class="fa fa-times b-like" title="Убрать из списка"></i> убрать лайк</a></p>
                                 </div>
                             </div>
                         <? endforeach;?>
-                        </div>
                     <? else :?>
                         <p>Вы еще не поставили ни одного "лайка"</p>
                     <? endif;?>
@@ -54,9 +51,10 @@
                                     <a href="/post/<?=$aComment['post_id']?>#comment_<?=$aComment['id']?>"><img class="media-object b-postthumb__small" src="<?=$aComment['thumb']?>" alt="<?=$aComment['title']?>"></a>
                                 </div>
                                 <div class="media-body">
-                                    <p class="media-heading"><a href="/post/<?=$aComment['post_id']?>#comment_<?=$aComment['id']?>"><?=$aComment['title']?></a>,
-                                        <span class="text-muted"><?=date2ru($aComment['cdate'])?> 
-                                    </p>
+                                    <h4 class="media-heading">
+                                        <a href="/post/<?=$aComment['post_id']?>#comment_<?=$aComment['id']?>"><?=$aComment['title']?></a>,
+                                        <span class="text-muted"><?=date2ru($aComment['cdate'], true)?></span>
+                                    </h4>
                                     <div class="b-comment__text">
                                         <?=nl2br(htmlspecialchars($aComment['text']));?>
                                     </div>
