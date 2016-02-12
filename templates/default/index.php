95c7ec8e3e184833b6b4ea4ae85ce8474c39cb5b
Vadim Suleimanov <s1imus05@gmail.com>
2016-02-12 23:43:26 +0300

    SSXY link added

--- templates/default/index.php
+++ templates/default/index.php
@@ -56,6 +56,7 @@
                             <? foreach ($aChapters as $aVal) :?>
                                 <li role="presentation" <?=isset($sChapter) && ($sChapter===$aVal['class'])?'class="active"':''?>><a href="/chapter/<?=$aVal['class']?>"><?=$aVal['title']?></a></li>
                             <? endforeach;?>
+                            <li role="presentation"><a href="http://ssxy.ru/" style="color: #a00;" onclick="return confirm('Контент только 18+! Подтвердите переход на другой сайт');">Для взрослых</a></li>
                             <li role="separator" class="divider"></li>
                             <? if ($bIsLogged) :?>
                                 <li role="presentation"><a href="/home">Личный кабинет</a>
@@ -94,6 +95,7 @@
                 <? foreach ($aChapters as $aVal) :?>
                     <li role="presentation" <?=isset($sChapter) && ($sChapter===$aVal['class'])?'class="active"':''?>><a href="/chapter/<?=$aVal['class']?>"><?=$aVal['title']?></a></li>
                 <? endforeach;?>
+                <li role="presentation"><a href="http://ssxy.ru/" style="color: #a00;" onclick="return confirm('Контент только 18+! Подтвердите переход на другой сайт');">Для взрослых</a></li>
             </ul>
         </nav>
         
